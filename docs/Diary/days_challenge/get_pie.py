05434dc47fb80367d24de016bd6ec0870cfe9ddc
XuebaStudy <xueba_320@qq.com>
2024-12-03 01:08:41 +0800

    test5.4

--- docs/Diary/days_challenge/get_pie.py
+++ docs/Diary/days_challenge/get_pie.py
@@ -1,42 +1,14 @@
-import matplotlib.pyplot as plt  
-import re  
-# from collections import defaultdict  
-from matplotlib import rcParams
-config = {
-        "font.family": 'serif',
-        "mathtext.fontset": 'stix',  # matplotlib渲染数学字体时使用的字体，和Times New Roman差别不大
-        "font.serif": ['FZYaoti'],
-        'axes.unicode_minus': False  # 处理负号，即-号
-    }
-rcParams.update(config)
-
-date = "24.11.7"
-data = """  
-概率论/review/80min
-背单词/R100/20min
-AI导/Lab/60min
-上课&背单词/定向越野&R110/100min
-运动/NM/20min
-休息/noon break/20min
-上课&AI导/NA&Lab/125min
-背单词/R90/25min
-科研/RP/90min
-概率论/HW/30min
-物理/HW/15min
-概率论/HW/15min
-博客/conclusion/20min
-"""  
-  
 # 定义固定颜色映射  
 activity_colors = {  
     '概率论': 'tab:green',  
     'pytorch': 'tab:orange',  
-    '背单词': 'tab:purple',  
+    'CET': 'tab:purple',  
     '数值分析': 'tab:brown',  
     '计组': 'tab:blue',  
     '科研': 'tab:cyan', 
     '休息': '#A9A9A9' ,
     '小提琴':'#D2691E',
+    '乐团':  '#D2691E',
     '上课': '#696969',
     'AI导':'#48D1CC',
     '运动':'#FFA500',
@@ -46,27 +18,43 @@ activity_colors = {
     '整理':'#DCDCDC',   # Gainsboro
     '博客':'#DCDCDC',
     
-    '上课&背单词':'#6A5ACD',  # SlateBlue
-    '上课&AI导':'#6A5ACD'
+    # '上课&背单词':'#6A5ACD',  # SlateBlue
+    # '上课&AI导':'#6A5ACD'
 }  
+
+from date_data import *
+import matplotlib.pyplot as plt  
+import re   
+from matplotlib import rcParams
+config = {
+        "font.family": 'serif',
+        "mathtext.fontset": 'stix',  # matplotlib渲染数学字体时使用的字体，和Times New Roman差别不大
+        "font.serif": ['FZYaoti'],
+        'axes.unicode_minus': False  # 处理负号，即-号
+    }
+rcParams.update(config)
   
 # 解析数据  
 activities = []  
 time_spent = []  
   
 # 正则表达式  
-pattern = r'(.+?)/(.+?)/(\d+)min'  
-  
+pattern = r'(.+?)/(.+?)/(\d+)min/(\d+)'  
+
+score_sum = 0 
 for line in data.strip().split('\n'):  
     match = re.match(pattern, line)  
     if match:  
         main_activity = match.group(1)  
-        minutes = int(match.group(3))  
+        minutes = int(match.group(3))
+        scores = int(match.group(4))  
           
         # 合并主活动和子活动为标签（如果需要）  
         label = f"{main_activity} ({match.group(2)})"  
         activities.append(label)  
-        time_spent.append(minutes)  
+        time_spent.append(minutes)
+        
+        score_sum += scores * minutes  
   
 # 创建饼状图，使用固定颜色  
 fig, ax = plt.subplots()  
@@ -86,3 +74,7 @@ ax.axis('equal')  # 确保饼状图是圆形
 plt.savefig("./docs/Diary/days_challenge/charts/pie-{}.png".format(date),)
 # 显示图表  
 # plt.show()
+
+se_min=60*(e_h-s_h)+(e_m-s_m)
+print('今日加权有效时长:',score_sum//60,'h',score_sum%60,'min')
+print('今日时间相对利用比: {:.2f} %'.format(score_sum/se_min*100))

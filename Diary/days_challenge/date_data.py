a838f7de88ed3c6b22ef1d730b1f7fc303c8bfa9
XuebaStudy <xueba_320@qq.com>
2024-12-03 01:09:46 +0800

    Deployed 05434dc with MkDocs version: 1.6.1

---
+++ Diary/days_challenge/date_data.py
@@ -0,0 +1,20 @@
+date = "24.12.2"
+
+s_h = 8
+s_m = 58
+e_h = 24
+e_m = 30
+
+data = """  
+CET/words/10min/1
+物理/lesson&review/120min/1
+休息/noon break/15min/1
+AI导/lesson&review/50min/1
+ADS/HW&review/70min/1
+乐团/rehearsal/130min/1
+数值分析/review&teamwork/50min/1
+物理/review/30min/1
+计组/lab/20min/1
+博客/conclusion/35min/1
+"""  
+ 

ac8feb82dfcef26a13fcdf8db93502dcd3871dbc
xueba <xueba_320@qq.com>
2025-04-16 11:37:04 +0800

    Date 2025.4.16

--- docs/index.md
+++ docs/index.md
@@ -1,20 +1,17 @@
 # **Welcome to xueba's notebook!**
 
-~~话说我应该没有在正式场合公开过我的笔记，我可不可以认为你已经认识我了~~
 
 !!! info "About me"
     <div style="text-align: center;font-size:18px;"  class="tightBlock">
     [ZJU](https://www.zju.edu.cn/)  23级 本科 Turing-CSer 一枚  
     自律进取，团结友善，  
-    提升自己，帮助他人，  
-    以让他人由衷认可[我的信仰](Belief/index.md)  
+    提升自己，帮助他人。  
+
       
-    “帮助过我的人好像信仰.....”  
-    “信仰......的人好像帮助过我”
     </div>
     ---
     - :simple-github: [我的 GitHub 账号](https://github.com/XuebaStudy)  
-    - :material-email: [我的邮箱](mailto:xuebajames@gmail.com)  
+    - :material-email: [我的邮箱](mailto:xuebajames@outlook.com)  
     
 
 
--- scripts/update_tree.py
+++ scripts/update_tree.py
@@ -38,58 +38,6 @@ if __name__ == "__main__":
     # yml文件内容注意正确缩进（且根级部分前没有"-"）
     yml_content = """
     
-  Lesson: 
-    - Lesson/index.md
-    - NJU ISC PA: Lesson/NJU ISC PA/Tasks.md
-    - Learning Path (in ZJU): Lesson/How_1.md
-    - Math:
-      - 常微分方程: Lesson/Math/ODE/note.md
-
-  Code: 
-    - Code/index.md
-    - C++: Code/C++/Cpp_note.md
-    - Python:
-      - matplotlib.pyplot: Code/Python/matplotlib.pyplot/1.md
-      - Mix:
-        - req_re_df_1: Code/Python/Mix/req_re_df_250209.md
-    - CUMCM:
-      - Code/CUMCM/index.md
-      - 2020年 A题: Code/CUMCM/20A.md
-    - Regex (正则表达式): Code/Regex.md
-
-  Research:
-    - Research/index.md
-    - 3DGS: 
-      - 3D Gaussian Splatting for Real-Time Radiance Field Rendering: Research/3DGS/1.md
-    - Quantization:
-      - (Survey) Model Quantization for DNN in Image Classification: Research/Quantization/1.md
-      - (Survey) Model Quantization and Hardware Acceleration for Vision Transformers: Research/Quantization/2.md
-    - Paper Tips: Research/paper_tips.md
-
-  Music:
-    - Music/index.md
-    - Music with places:
-      - Music/places/index.md
-    - Others: 
-      - mp3->mp4 (有声频谱): Music/Others/mp3_visualization.md
-
-  Tool:
-    - Tool/index.md
-    - Mkdocs: 
-      - Commands: Tool/mkdocs/commands.md
-      - Others: Tool/mkdocs/others.md
-    - Linux:
-      - Terminal: Tool/Linux/commands.md
-      - 基本配置: Tool/Linux/Linux_setup.md
-      - Vim: 
-        - Tool/Linux/Vim/tips.md
-      - Others: Tool/Linux/others.md
-    - Git: Tool/git.md
-    - Adobe:
-      - PS shortcuts: Tool/Adobe/PS_shortcut.md
-    - Others: 
-      - Terminal:如何使"命令"显示在"路径"下一行: Tool/Others/terminal_1.md
-
   Diary: 
     - Diary/index.md
     - Reflection: 
@@ -102,25 +50,11 @@ if __name__ == "__main__":
       - experience: Diary/Research/experience.md
     - Days_Challenge: 
       - Diary/days_challenge/index.md
-      - '24.8': Diary/days_challenge/24.8.md
-      - '24.10': Diary/days_challenge/24.10.md
-      - '24.11': Diary/days_challenge/24.11.md
-      - '24.12': Diary/days_challenge/24.12.md
-      - '25.2': Diary/days_challenge/25.2.md
-      - '25.2~3': Diary/days_challenge/25.2_2.md
-      - '25.4': Diary/days_challenge/25.4.md
     - Thinking:
       - Diary/Thinking/index.md
       - '24.12.5': Diary/Thinking/24.12.5.md
       - 'Things' : Diary/Thinking/things.md
-
-  Belief:  
-    - Belief/index.md
-    - charators: 
-      - 灰之魔女伊蕾娜: Belief/charactors/灰之魔女伊蕾娜.md
-      - 洛琪希: Belief/charactors/洛琪希.md
-    - sentences: Belief/sentences.md
-
+      
     """
 
     update_all_indexes(yml_content, update_time, base_output_dir)
